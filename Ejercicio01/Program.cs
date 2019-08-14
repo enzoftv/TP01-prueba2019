8ce835dc46056e3706f578933c17763437275be5
Enzo <eftanga@gmail.com>
2019-08-14 17:51:02 -0300

    Add project files.

---
+++ Ejercicio01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Ingrese una palabra:");
+            //int numero = Convert.ToInt32( Console.ReadLine());
+
+            //Console.WriteLine("La palabra leida es :" + numero);
+            //Console.ReadLine();
+
+            int valor = 1;
+
+            switch(valor)
+            {
+                case 1:
+                    Console.WriteLine("1");
+                    break;
+                case 2:
+                    Console.WriteLine("2");
+                   break;
+                default:
+                    Console.WriteLine("d");
+                    break;
+
+            }
+        }
+    }
+}
